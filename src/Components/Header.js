672068388afeef5d68e94c8cea7fabf8ce2114cd
brahamSociomonkey <74758151+brahamSociomonkey@users.noreply.github.com>
2024-04-24 13:19:43 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -84,7 +84,7 @@ export const Header = () => {
                   >
                    MULETHI
                   </Link>*/}
-                      <a href="https://mulethi.in/" target="_blank">MULETHI</a>
+                      <a href="https://mulethi.in/">MULETHI</a>
                 </li>
                 
               </ul>
